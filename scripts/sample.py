de5cc8e008fc213220277233e02c013546e519e9
ameldocena <ameldocena3@gmail.com>
2023-06-27 16:48:37 -0400

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -12,25 +12,28 @@ class Navigator:
         print("we got inside")
 
         #Service client to GetPlan
-        #Interesting: For multi-robot, shouldn't this be robot-specific; i.e., either robot 0 or 1
-        #But if we rossrv list there is only nav_msgs/GetPlan
-        rospy.wait_for_service('nav_msgs/GetPlan')
-        self.get_plan = rospy.ServiceProxy('nav_msgs/GetPlan', GetPlan)
+        self.get_plan_services = dict()
+        for robot_id in range(nrobots):
+            server = '/robot_'+str(robot_id)+'/move_base_node/make_plan'
+            rospy.wait_for_service(server)
+            self.get_plan_services['robot'+str(robot_id)] = rospy.ServiceProxy(server, GetPlan)
 
         #Send robot to goal
         #Option 1: Action client to move base
         #Robot 0 or 1 for nrobots=2
         self.robot_goal_clients = dict()
         for robot_id in range(nrobots):
-            self.robot_goal_clients['robot'+str(robot_id)] = actionlib.SimpleActionClient('/robot_'+str(robot_id)+'/move_base/goal', MoveBaseAction)
+            self.robot_goal_clients['robot'+str(robot_id)] = actionlib.SimpleActionClient('/robot_'+str(robot_id)+'/move_base', MoveBaseAction)
             self.robot_goal_clients['robot'+str(robot_id)].wait_for_server()
 
         #Option 2: Subscribed topic
-        self.robot_goal_topics = dict()
-        for robot_id in range(nrobots):
-            self.robot_goal_topics['robot'+str(robot_id)] = rospy.Publisher('/robot_'+str(robot_id)+'/move_base_simple/goal', PoseStamped, queue_size=1)
+        # self.robot_goal_topics = dict()
+        # for robot_id in range(nrobots):
+        #     self.robot_goal_topics['robot'+str(robot_id)] = rospy.Publisher('/robot_'+str(robot_id)+'/move_base_simple/goal', PoseStamped, queue_size=1)
 
-    def get_plan_cb(self, start_pose, goal_pose, tolerance):
+        print("Constructed")
+
+    def get_plan_request(self, robot_id, start_pose, goal_pose, tolerance):
         """
         Sends a request to GetPlan service to create a plan for path from start to goal without actually moving the robot
         :param start_pose:
@@ -42,10 +45,11 @@ class Navigator:
         req.start = start_pose
         req.goal = goal_pose
         req.tolerance = tolerance
-        resp = self.get_plan(req.start, req.goal, req.tolerance)
+        server = self.get_plan_services['robot'+str(robot_id)]
+        resp = server(req.start, req.goal, req.tolerance)
         return resp
 
-    def send_robot_goal_cb(self, robot_id, goal):
+    def send_robot_goal(self, robot_id, goal):
         """
         Sends robot to goal via action client
         :param robot:
@@ -56,10 +60,19 @@ class Navigator:
 
         movebase_goal = MoveBaseGoal()
         movebase_goal.target_pose = goal
-        client.send_goal(movebase_goal)
-        client.wait_for_result()
-        result = client.get_result()
-        return result
+        print("Movebase goal:", movebase_goal)
+        action_goal_cb = (lambda msg : self.action_send_done_cb(msg, robot_id))
+        client.send_goal(movebase_goal, done_cb=action_goal_cb)
+
+    def action_send_done_cb(self, msg, robot_id):
+        """
+
+        :param msg:
+        :return:
+        """
+        #Note: Currently there is a tiny bug on the lambda
+        print("Robot id: {}. Msg: {}".format(robot_id, msg))
+
 
     def pub_robot_goal_cb(self, robot_id, goal):
         """
@@ -79,33 +92,37 @@ class Navigator:
 
 
 if __name__== '__main__':
+    navigator = Navigator('sample_nav', 2)
+
     start = PoseStamped()
     start.header.seq = 0
     start.header.frame_id = "map"
-    start.header.stamp = rospy.Time(0)
+    start.header.stamp = rospy.Time.now()
     start.pose.position.x = 0.0
     start.pose.position.y = 0.0
+    start.pose.orientation.w = 1.0
 
     goal = PoseStamped()
     goal.header.seq = 0
     goal.header.frame_id = "map"
-    goal.header.stamp = rospy.Time(0)
+    goal.header.stamp = rospy.Time.now()
     goal.pose.position.x = 2.0
     goal.pose.position.y = 2.0
+    goal.pose.orientation.w = 1.0
 
-    navigator = Navigator('sample_nav', 2)
 
     #Subscribe to GetPlan service
     #Uncomment below
     # tolerance = 0.5
-    # plan = navigator.get_plan_cb(start, goal, tolerance)
+    # plan = navigator.get_plan_request(0, start, goal, tolerance)
     # print(plan)
 
     #Action client to move_base: Send robot to goal
     #Uncommment below
-    # navigator.send_robot_goal_cb(0, goal)
+    result = navigator.send_robot_goal(0, goal)
+    print("Sent goal", result)
 
     #Publish topic to send robot to goal
-    navigator.pub_robot_goal_cb(0, goal)
+    # navigator.pub_robot_goal_cb(0, goal)
 
     navigator.spin()
